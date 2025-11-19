26cb635eecd3fa44926742e455eb2987a5e99968
w3-80717 <chavan1609@gmail.com>
2025-11-19 21:04:16 +0530

    Quiz Handling to the student or user and Admin added Quiz and Questions

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import authRoutes from "./routes/auth.routes.js";
 import courseRoutes from "./routes/course.routes.js"
 import categoryRoutes from "./routes/category.routes.js"
 import batchRoutes from "./routes/batch.routes.js"
+import quizRoutes from "./routes/quiz.routes.js"
+import quizQuestionRoutes from "./routes/quiz_question.routes.js";
+import quizAttemptRoutes from "./routes/quiz_attempt.routes.js";
 import { protect } from "./middlewares/auth.middleware.js";
 // import uploadRoutes from "./routes/uploadRoutes.js";
 import "./models/index.js";   // VERY IMPORTANT
@@ -17,6 +20,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/course", courseRoutes)
 app.use('/api/category', categoryRoutes);
 app.use('/api/batch', batchRoutes)
+app.use('/api/quiz', quizRoutes)
+app.use("/api/quiz-questions", quizQuestionRoutes);
+app.use("/api/quiz-attempts", quizAttemptRoutes); 
 
 app.get("/api/user/profile", protect, (req, res) => {
   res.json({ message: "Welcome to your profile!", userId: req.user.id });
