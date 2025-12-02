d1c891f173f89228a73f7a54e4af65942ddbe072
Rohit Watharkar <watharkarrohit7975@gmail.com>
2025-12-02 17:35:08 +0530

    changes all done also add forgot,reset,change password

--- src/models/video.model.js
+++
@@ -1,8 +0,0 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../config/db.js";
-
-export const Video = sequelize.define("Video", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  title: DataTypes.STRING,
-  url: DataTypes.STRING,
-});
--- src/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-// import { uploadFile } from "../controllers/uploadController.js";
-// import upload from "../middlewares/upload.middleware.js";
-
-// const router = express.Router();
-
-// router.post("/upload", upload.single("file"), uploadFile);
-
-// export default router;
