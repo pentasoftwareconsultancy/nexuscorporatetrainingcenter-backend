6e3861c591dd26e269a2289fa4c098641736168e
Rohit Watharkar <watharkarrohit7975@gmail.com>
2025-12-15 17:56:23 +0530

    changes made

--- src/controllers/master/master.controller.js
+++ src/controllers/master/master.controller.js
@@ -100,6 +100,7 @@ const masterController = {
   createCourseDetails: async (req, res) => {
     try {
       const data = await masterService.createCourseDetails(req.body, req.file);
+
       res.json({ success: true, data });
     } catch (err) {
       res.status(400).json({ success: false, message: err.message });
