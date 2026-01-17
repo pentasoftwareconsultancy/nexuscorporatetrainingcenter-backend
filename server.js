29258ef2326611fbe1342fef8f3bb1679845da2a
Ajinkya Sasne <ajinkya.sasne@gmail.com>
2026-01-17 11:29:41 +0530

    changes made

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ import { setupUserTestHooks } from "./src/hooks/userTest.hooks.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://nexus10v.vercel.app",
+];
+
 app.use(
-  cross({
-    origin: "http://localhost:5173", // your React frontend URL
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow Postman / server-to-server
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // only needed if you use cookies
+    credentials: true,
   })
 );
 
