80bde876de318aef6d750cdc6caf9d3b7952e917
Ajinkya Sasne <ajinkya.sasne@gmail.com>
2026-01-17 11:43:34 +0530

    changes made

--- server.js
+++ server.js
@@ -1,32 +1,11 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
 import { sequelize } from "./src/config/db.js";
-import cors from "cors";
 import { setupAssociations } from "./src/models/comman/associations.js";
 import { setupUserTestHooks } from "./src/hooks/userTest.hooks.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://nexus10v.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow Postman / server-to-server
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
 
 const start = async () => {
   try {
